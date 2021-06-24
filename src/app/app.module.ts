59c9f218cde73e2c70ccce839a06db6867f79f03
Luis Montoya <luisc1999lm@gmail.com>
2021-06-23 20:13:45 -0500

    Cargando componentes Suma y Division

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { SumaComponent } from './suma/suma.component';
+import { DivisionComponent } from './division/division.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SumaComponent,
+    DivisionComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/division/division.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-division',
+  templateUrl: './division.component.html',
+  styleUrls: ['./division.component.css']
+})
+export class DivisionComponent{
+
+  division = "Division"
+
+}
